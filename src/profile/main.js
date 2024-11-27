681af2af0f844650c26d75a5ab26dd1201c0814e
black-rustx <robinhood.k110@gmail.com>
2024-11-27 18:33:29 +0900

    fix: hidden core code

--- src/profile/main.js
+++
@@ -1,9 +0,0 @@
-import chalk from 'chalk';
-import signIn from './login.js';
-
-async function createPumpProfiles() {
-    await signIn();
-    console.log(chalk.green('Completed sign-in process.'));
-}
-
-export default createPumpProfiles;
